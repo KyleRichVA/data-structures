cde0be21278ca3054030abe32c954effa70e966e
Kyle R <KyleRichVA@gmail.com>
2016-04-18 14:18:07 -0700

    tested for collisions.

--- src/test_hash.py
+++ src/test_hash.py
@@ -28,3 +28,18 @@ def test_set_base(test_hash):
 def test_set_non_string(test_hash, bads):
     with pytest.raises(TypeError):
         test_hash.set(bads, 34)
+
+
+def test_set_collision(test_hash):
+    test_hash.set(u'xz', 34)
+    test_hash.set(u'yy', 25)
+    # These two keys should have the same hash table index
+    assert test_hash._table[2] == [(u'xz', 34), (u'yy', 25)]
+
+
+def test_set_overwrite(test_hash):
+    test_hash.set(KEY, 34)
+    assert (KEY, 34) in test_hash._table[test_hash._hash(KEY)]
+    test_hash.set(KEY, 100)
+    assert (KEY, 34) not in test_hash._table[test_hash._hash(KEY)]
+    assert (KEY, 100) in test_hash._table[test_hash._hash(KEY)]
