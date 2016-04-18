8421f6a2668b9977cd0f5f0c6ff4a70d4340572f
Kyle R <KyleRichVA@gmail.com>
2016-04-18 13:41:56 -0700

    switched to hash table fixture and tested _hash method.

--- src/hash_table.py
+++ src/hash_table.py
@@ -5,3 +5,10 @@ class Hash_Table(object):
 
         Length of Table is Based on Passed Value num_buckets."""
         self._table = [None] * num_buckets
+
+    def _hash(self, key):
+        """Return the index of the table based off the hashed key."""
+        indx = 0
+        for char in key:
+            indx += ord(char)
+        return indx % len(self._table)
--- src/test_hash.py
+++ src/test_hash.py
@@ -1,6 +1,18 @@
 from hash_table import Hash_Table
+import pytest
+
+@pytest.fixture()
+def test_hash():
+    return Hash_Table(10)
 
 
-def test_hash_init():
-    test_hash = Hash_Table(25)
-    assert len(test_hash._table) == 25
+def test_hash_init(test_hash):
+    assert len(test_hash._table) == 10
+
+
+def test_hash_hasher(test_hash):
+    assert test_hash._hash('key') in range(len(test_hash._table))
+
+
+def test_set_base(test_hash):
+    pass
