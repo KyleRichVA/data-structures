acdc4b5b0a5aad4490a482b7bba6b2bafbca6a71
Kyle R <KyleRichVA@gmail.com>
2016-04-20 15:10:35 -0700

    Added and tested out sort_quick

---
+++ src/sort_quick.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Quick sort module."""
+import time
+from random import randint
+
+
+def sort(lst):
+    """Quick sort function."""
+    if len(lst):
+        Pindx = len(lst) >> 1
+    # set pivot as the middle of the list
+    pivot = [lst[Pindx]]
+    temp_left = lst[:Pindx]
+    temp_right = lst[Pindx+1:]
+    sort_left = []
+    sort_right = []
+    while len(temp_left):
+        if temp_left[-1] <= pivot[0]:
+            sort_left.append(temp_left.pop())
+        else:
+            sort_right.append(temp_left.pop())
+    while len(temp_right):
+        if temp_right[-1] <= pivot[0]:
+            sort_left.append(temp_right.pop())
+        else:
+            sort_right.append(temp_right.pop())
+    if len(sort_left):
+        sort_left = sort(sort_left)
+    if len(sort_right):
+        sort_right = sort(sort_right)
+    return sort_left + pivot + sort_right
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Insertion sort will iterate over a list comparatively sorting items from lowest to highest value.")
+    print("For example, if given [3, 1, 4, 2] the expected output will be [1, 2, 3, 4]")
+    print("Best case scenario (pre-sorted) [1, 2, 3, 4] the efficiency will be O(n)")
+    print("Worst case scenario (reverse-sorted) [4, 3, 2, 1] the efficiency will be O(n^2)")
+
+    print("Best Case Test:  list over range of 1-1000")
+    lst_best = list(range(1000))
+    start_best = time.time()
+    sort(lst_best)
+    end_best = time.time()
+    diff_best = end_best - start_best
+    print("Elapse time:  {} seconds".format(diff_best))
+
+    print("Average Case Test:  list over range of 1-1000 with random order of values")
+    lst_ave = [randint(0, 1000) for i in range(1000)]
+    start_ave = time.time()
+    sort(lst_ave)
+    end_ave = time.time()
+    diff_ave = end_ave - start_ave
+    print("Elapse time:  {} seconds".format(diff_ave))
+
+    print("Worst Case Test:  list over range of 1-1000 in reverse order (100-1)")
+    lst_worst = list(range(1000))[::-1]
+    start_worst = time.time()
+    sort(lst_worst)
+    end_worst = time.time()
+    diff_worst = end_worst - start_worst
+    print("Elapse time:  {} seconds".format(diff_worst))
---
+++ src/test_sort_quick.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Test file for insertion sort."""
+
+
+# LIST_NUM_SORTED = range(10)
+LIST_NUM_SORTED = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+LIST_NUM_SORTED2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+LIST_NUM_UNSORTED = [6, 2, 7, 1, 5, 3, 8, 4, 9, 0]
+LIST_NUM_REVERSE = LIST_NUM_SORTED[::-1]
+
+
+def test_best_case():
+    from sort_quick import sort
+    """Test best case sort. (pre-sorted)"""
+    assert sort(LIST_NUM_SORTED2) == LIST_NUM_SORTED
+
+
+def test_chaos_case():
+    from sort_quick import sort
+    """Test random case sort. (chaos sorted)"""
+    assert sort(LIST_NUM_UNSORTED) == LIST_NUM_SORTED
+
+
+def test_worse_case():
+    from sort_quick import sort
+    """Test worst case sort. (revrse-sorted)"""
+    assert sort(LIST_NUM_REVERSE) == LIST_NUM_SORTED
