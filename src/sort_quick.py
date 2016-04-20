1eaee613bbdfd3eec4da0996b64b8237f11dc515
Kyle R <KyleRichVA@gmail.com>
2016-04-20 15:16:03 -0700

    fixed up main block."

--- src/sort_quick.py
+++ src/sort_quick.py
@@ -31,34 +31,30 @@ def sort(lst):
     return sort_left + pivot + sort_right
 
 
-
-
-
-
 if __name__ == "__main__":
-    print("Insertion sort will iterate over a list comparatively sorting items from lowest to highest value.")
+    print("Quick sort will split the list based off comparison to a pivot point into 2 lists. These lists are recursively sorted and eventually connected back to pivot.")
     print("For example, if given [3, 1, 4, 2] the expected output will be [1, 2, 3, 4]")
-    print("Best case scenario (pre-sorted) [1, 2, 3, 4] the efficiency will be O(n)")
-    print("Worst case scenario (reverse-sorted) [4, 3, 2, 1] the efficiency will be O(n^2)")
+    print("Best case scenario (pre-sorted) [1, 2, 3, 4] the efficiency will be O(n log n)")
+    print("Worst case scenario (reverse-sorted) [4, 3, 2, 1] the efficiency will be O(n)")
 
-    print("Best Case Test:  list over range of 1-1000")
-    lst_best = list(range(1000))
+    print("Best Case Test:  list over range of 1-10000")
+    lst_best = list(range(10000))
     start_best = time.time()
     sort(lst_best)
     end_best = time.time()
     diff_best = end_best - start_best
     print("Elapse time:  {} seconds".format(diff_best))
 
-    print("Average Case Test:  list over range of 1-1000 with random order of values")
-    lst_ave = [randint(0, 1000) for i in range(1000)]
+    print("Average Case Test:  list over range of 1-10000 with random order of values")
+    lst_ave = [randint(0, 1000) for i in range(10000)]
     start_ave = time.time()
     sort(lst_ave)
     end_ave = time.time()
     diff_ave = end_ave - start_ave
     print("Elapse time:  {} seconds".format(diff_ave))
 
-    print("Worst Case Test:  list over range of 1-1000 in reverse order (100-1)")
-    lst_worst = list(range(1000))[::-1]
+    print("Worst Case Test:  list over range of 1-10000 in reverse order (100-1)")
+    lst_worst = list(range(10000))[::-1]
     start_worst = time.time()
     sort(lst_worst)
     end_worst = time.time()
